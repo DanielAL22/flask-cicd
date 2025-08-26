b2e832450c5ed1dc1997562515e9d4023d0ef9b9
Daniel <daniel.alm92@gmail.com>
2025-08-25 22:10:03 -0400

    se actualiza app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "¡Hola desde Flask en Raspberry Pi!"
+    return "¡Hola desde Flask en Raspberry Pi accesible desde internet y actualizado con un pipeline CD!"
